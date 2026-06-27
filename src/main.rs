d35b750898ed80707f04d0a5df93803063e4ae47
Robert Babaev <github@robertbabaev.tech>
2026-06-26 22:09:43 -0400

    build: flesh out dependencies and functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    name: Option<String>,
+}
+
+// What do I actually want to do
+// 1. Create global capabilities
+// 2. Create per-agent capabilities
+// 3. Sync with a watchdog?
+
 fn main() {
     println!("Hello, world!");
 }
